c43a5fbf69eab88ea4be4b2b107a7dfa03d37169
Dr Nic Williams <drnicwilliams@gmail.com>
2012-11-20 17:03:43 -0800

    initial my-github-projects script

---
+++ lib/my-github-projects.rb
@@ -0,0 +1,9 @@
+require "my-github-projects/version"
+
+module My
+  module Github
+    module Projects
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/my-github-projects/version.rb
@@ -0,0 +1,7 @@
+module My
+  module Github
+    module Projects
+      VERSION = "0.0.1"
+    end
+  end
+end
